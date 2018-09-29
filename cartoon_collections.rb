4fa5312828f7226875177d6bb24b4b6422ca838b
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 21:10:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |dwarf, index|
     index += 1
-    puts "#{index}. #{dwarf}
+    puts "#{index}. #{dwarf}"
   end
 
 end
